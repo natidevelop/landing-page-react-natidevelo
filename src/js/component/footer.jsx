ef95e2b2da65b2a02925a8aad944048d8bf0ea94
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-03 17:53:04 +0000

    mi landpage

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <>
+            <nav className="navbar bg-dark mt-3">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Natali -2024</span>
+                </div>
+            </nav>
+
+        </>
+
+    )
+}
+export default Footer
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,33 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="container text-center mt-3">
+				<div className="row">
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+
+				</div>
+			</div>
+			<Footer />
+
 		</div>
 	);
 };
